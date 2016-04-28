6c51f8c904cd600ff7cb6395ca691bf2573a8926
santiagofw <santiago@devsar.com>
2016-04-28 11:05:23 -0300

    Changes to dockerfiles

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-pip install -r requirements.txt
-gunicorn -b 0.0.0.0:8000 app
+pip install -r /src/requirements.txt
+cd /src &&  gunicorn -b 0.0.0.0:8000 app
--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,4 @@
 import os
 
 MONGO_HOST = os.getenv('MONGO_HOST', '127.0.0.1')
-MONGO_PORT = os.getenv('MONGO_PORT', 27017)
+MONGO_PORT = int(os.getenv('MONGO_PORT', 27017))
